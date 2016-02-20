acee87ff8693552cf5c7604c5302b4e9c3b85685
Ari Munandar <arimunandr.dev@gmail.com>
2016-02-20 12:24:05 +0700

    iOS Project Starter

---
+++ iOS-Tutorials/iOS-Tutorials/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  iOS-Tutorials
+//
+//  Created by Innovecto iOS Dev on 2/20/16.
+//  Copyright © 2016 Devcast. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        
+        // MARK : Set Frame
+    }
+
+}
+
